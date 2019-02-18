b9b098ad6c834715b973308a1ecdd5c6fa9ad4f8
hungari97 <vagonora@gmail.com>
2019-02-18 09:14:35 -0800

    tachograph added

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,8 +1,11 @@
 package hu.bme.mit.train.sensor;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 import static org.mockito.Mockito.*;
 
@@ -15,11 +18,25 @@ public class TrainSensorTest {
 
     @Test
     public void ThisIsAnExampleTestStub() {
+
     }
 
     @Test
     public void AnotherTestStub() {
         assertTrue(true);
+    }
+
+    @Test
+    public void TachographStub() {
+        Tachograph tachograph =new Tachograph();
+        //Assert.assert(tachograph.table.isEmpty());
+
+        if(tachograph.table.isEmpty()){
+        assertFalse(false);
+        }
+        else {
+            assertTrue(true);
+        }
     }
 
 }
