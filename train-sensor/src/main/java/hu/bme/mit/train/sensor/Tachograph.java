bd23d3ff157ff8d503831541a5e3cf49b0f8ae32
honoruis <jakabrichard23@gmail.com>
2019-04-08 06:25:18 -0700

    Revert "tachograph added"
    This reverts commit b9b098ad6c834715b973308a1ecdd5c6fa9ad4f8.

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/Tachograph.java
+++
@@ -1,9 +0,0 @@
-package hu.bme.mit.train.sensor;
-
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
-
-
-public class Tachograph {
-    public Table<Double, Double, Double> table = HashBasedTable.create();
-}
